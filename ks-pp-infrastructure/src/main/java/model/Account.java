1b0c1f30c684eaa3704ae3fe08fe86a2e0a60cc1
He Zhou <zhouhegu@gmail.com>
2022-03-04 00:29:54 +0800

    model

---
+++ ks-pp-infrastructure/src/main/java/model/Account.java
@@ -0,0 +1,9 @@
+package model;
+
+/**
+ * @author gupp
+ * @date 2022/3/4 12:27 上午
+ */
+public class Account {
+
+}
---
+++ ks-pp-infrastructure/src/main/java/model/Agent.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * @author gupp
+ * @date 2022/3/4 12:29 上午
+ */
+public class Agent {
+}
