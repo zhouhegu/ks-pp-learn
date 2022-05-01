93881ac4139640582ac03ff01a30080747d340b2
He Zhou <zhouhegu@gmail.com>
2022-05-01 16:24:48 +0800

    update

---
+++ ks-pp-learn-basic/src/main/java/com/pp/basic/lambda/Main.java
@@ -0,0 +1,17 @@
+package com.pp.basic.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author gupp
+ * @date 2022/3/19 4:27 下午
+ */
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        System.out.println(list);
+        System.out.println(list.stream().filter(num -> num < 5).collect(Collectors.toList()));
+    }
+}
---
+++ ks-pp-learn-basic/src/main/java/com/pp/basic/lambda/Operator.java
@@ -0,0 +1,10 @@
+package com.pp.basic.lambda;
+
+/**
+ * @author gupp
+ * @date 2022/3/19 4:27 下午
+ */
+public interface Operator {
+
+    int act(int a, int b);
+}
