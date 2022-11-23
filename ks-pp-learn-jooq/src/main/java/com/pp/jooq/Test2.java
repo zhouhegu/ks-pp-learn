965829737606c09d65ef113bd7aad71b0c062dd7
He Zhou <zhouhegu@gmail.com>
2022-11-23 17:08:05 +0800

    ddd

---
+++ ks-pp-learn-jooq/src/main/java/com/pp/jooq/Test2.java
@@ -0,0 +1,11 @@
+package com.pp.jooq;
+
+/**
+ * @author gupp
+ * @date 2022/11/23 17:07
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
